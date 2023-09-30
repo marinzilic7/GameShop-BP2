d9ee8f3d4034fc0f6a9ac483e0f1528ca0f0a000
Contineo <marinzilic507@gmail.com>
2023-09-30 14:18:18 +0200

    update

--- model/__init__.py
+++ model/__init__.py
@@ -1,10 +1,10 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 
-engine = create_engine("mysql+pymysql://root@localhost:3306/gameshop")
+""" engine = create_engine("mysql+pymysql://root@localhost:3306/gameshop") """
 
 
-""" engine = create_engine("mysql+pymysql://studio:studio123@mysql:3306/studio")  """
+engine = create_engine("mysql+pymysql://gameshop:gameshop123@mysql:3306/gameshop") 
 Session = sessionmaker(bind=engine)
 
 Session = Session()
